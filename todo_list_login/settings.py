38f66623e5dfa9b516a04587f6371b2ad657c7b3
michel jean <michel.jean.uk@gmail.com>
2021-09-04 10:27:08 +0100

    deploy

--- todo_list_login/settings.py
+++ todo_list_login/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i@y59bm$ujip9$u2^)lmm0@94pk+ez4=!r@31qg_#+shbsw@^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mjtodolist-login.herokuapp.com/', '127.0.0.1']
 
